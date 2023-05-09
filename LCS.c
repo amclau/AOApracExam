a82aa840f40d0651458dac010e526a9ff1127fff
amclau <98709499+amclau@users.noreply.github.com>
2023-05-09 07:49:38 +0000

    LCS added

--- LCS.c
+++ LCS.c
@@ -4,5 +4,17 @@
 #include <stdlib.h>
 void main()
 {
-    
+    char x[4]='ABCD';
+    char y[4]='AEDF';
+
+    int dp[5][5]={{0}};
+
+    for(int i=0;i<4;i++)
+    {
+for (int i = 1; i < 4; i++)
+{
+
+}
+
+    }
 }
